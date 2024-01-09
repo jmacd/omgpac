a9768703e935fe734fd4c969eebe3237cd155b50
Joshua MacDonald <jmacd@lightstep.com>
2024-01-08 20:39:05 -0800

    initial

---
+++ src/main/java/art/lookingup/patterns/pacman/Colors.java
@@ -0,0 +1,13 @@
+package art.lookingup.colors;
+
+public final class Colors {
+
+  public static final int BLACK = 0xff000000;
+  public static final int RED = 0xffff0000;
+  public static final int ORANGE = 0xffff7f00;
+  public static final int YELLOW = 0xffffff00;
+  public static final int GREEN = 0xff00ff00;
+  public static final int BLUE = 0xff0000ff;
+  public static final int VIOLET = 0xff8b00ff;
+  public static final int WHITE = 0xffffffff;
+}
